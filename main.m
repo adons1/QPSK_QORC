f28db4d59b7d019fb5b44dfa9da6a6be26af4ac2
adons1 <ant.adons@gmail.com>
2021-12-22 01:12:34 +0300

    костыль

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clear; clc; close all;
 
 res= zeros(1,4);
-N=10^6;
+N=10^5;
 Tb=10^-4;
 binary = randi([0 1],N,1);
 
@@ -24,13 +24,9 @@ SNR_for_QORC=(0:1.0:15.0)-7.0;
 [errsQORC]=model(modemQORC, signalQORC, SNR_for_QORC);
 toc
 
- figure
-% hold on
+figure
 semilogy(SNR-10*log10(2), errsQPSK, SNR-10*log10(2), errsQORC);
-% plot(SNR-10*log10(2), log10(errsQPSK));
-% plot(SNR+10*log10(2), log10(errsQORC));
 legend('QPSK','QORC');
-% hold off
 
 
 
